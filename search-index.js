3b7b727f927044ae4b10d09b2689ee05e29f15c4
ghost <ghost@konpa.ku>
2016-07-09 19:43:53 +0000

    0.0.2 - 20603d14247328d3590898480b30c479885841d6

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["uninitialized"] = {"doc":"Provides the option to use uninitialized memory for performance improvements.","items":[[5,"uninitialized","uninitialized","Creates a value initialized to zero.",null,{"inputs":[],"output":{"name":"t"}}],[17,"UNINITIALIZED","","A constant indicating whether the `uninitialized` feature is enabled.",null,null]],"paths":[]};
+initSearch(searchIndex);
